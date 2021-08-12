6dc7d2e40c24c2e28e24b74697f199e7d39f1471
sanaz <szeraati69@gmail.com>
2021-08-12 17:34:59 +0430

    add exercise 6

---
+++ Week1/homework/Ex6-LoganArrayofanimal.js
@@ -0,0 +1,7 @@
+let animals = [];
+console.log("array contains animals");
+console.log(animals);
+let newanimals = ["dog", "horse", "cat"];
+console.log(newanimals);
+(newanimals.push("piglet"));
+console.log(newanimals);
