1f107819503c6d931b8c09f6a0b280d329b52b39
sanaz <szeraati69@gmail.com>
2021-09-03 20:19:53 +0430

    add ex4-TheReadingBooks

---
+++ week2/homework/Ex4-TheReadingList.js
@@ -0,0 +1,23 @@
+const books = [
+    {title: 'The Forty Rules of Love',
+     author: 'Elif Shafak',
+     alreadyRead: true
+    },
+    {title: 'Blindness',
+     author: 'José Saramago',
+     alreadyRead: true
+    },
+    {title: 'The Feeling Good',
+     author: 'David D. Burns',
+     alreadyRead: false
+    }];
+
+for(let i = 0; i < books.length; i++){
+    console.log('The ' + books[i].title + ' by ' + books[i].author);
+    
+    if (books[i].alreadyRead === true) {
+        console.log('You already read ' + books[i].title);
+      } else {
+        console.log('You still need to read ' + books[i].title);
+      }
+}
