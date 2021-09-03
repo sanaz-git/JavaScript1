7808c18c3cf92a945532d142ef0fcc2d97656f80
sanaz <szeraati69@gmail.com>
2021-09-03 17:43:27 +0430

    complete ex2

--- week2/homework/Ex2-EvenorOdd-reporter.js
+++ week2/homework/Ex2-EvenorOdd-reporter.js
@@ -1,10 +1,9 @@
-const numbers = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
 
 for(let i=0 ; i<21; i++){
-    if(numbers%2 == 0){
-        console.log('The number' + numbers + 'is odd! .')
+    if(i%2 == 0){
+        console.log('The number ' + i + ' is even! .')
     }else{
-        console.log('The number' + numbers + 'is even! .')
+        console.log('The number ' + i + ' is odd! .')
     }
 }
 
