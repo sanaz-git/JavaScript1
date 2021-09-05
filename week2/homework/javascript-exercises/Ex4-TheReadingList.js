a140db2b951500ba30dcd1fddd625cc03c523cd9
sanaz <szeraati69@gmail.com>
2021-09-05 21:03:48 +0430

    add temperture-convertot

--- week2/homework/javascript-exercises/Ex4-TheReadingList.js
+++ week2/homework/javascript-exercises/Ex4-TheReadingList.js
@@ -15,7 +15,7 @@ const books = [
 for(let i = 0; i < books.length; i++){
     console.log('The ' + books[i].title + ' by ' + books[i].author);
     
-    if (books[i].alreadyRead === true) {
+    if (books[i].alreadyRead == true) {
         console.log('You already read ' + books[i].title);
       } else {
         console.log('You still need to read ' + books[i].title);
---
+++ week2/homework/javascript-exercises/Ex5-WhoWantsaDrink.js
@@ -0,0 +1,9 @@
+// It hasn't completed yet
+const drinkTray = [];
+const drinkTypes = ['cola', 'lemonade', 'water'];
+drinkTray.push('cola', 'lemonade', 'water');
+console.log(drinkTray);
+
+for(let i = 0; i<= 5; i++){
+    
+}
