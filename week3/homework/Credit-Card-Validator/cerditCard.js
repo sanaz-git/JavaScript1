fa8cfbd638ee8b0b307de25ecf6050cff0817216
sanaz <szeraati69@gmail.com>
2021-10-05 19:06:47 +0330

    add project

--- week3/homework/Credit-Card-Validator/cerditCard.js
+++ week3/homework/Credit-Card-Validator/cerditCard.js
@@ -1,3 +1,5 @@
+// const num = prompt("Enter a number: ");
+
 
 // Input must be 16 characters
 function numCharacters(num){
@@ -29,8 +31,7 @@ function checkNumber(num){
 console.log(checkNumber("1234567345678"));
 
 
-//At least two different numbers should be represented
-
+// At least two different numbers should be represented
 function twoDifferentNumbers(num){
     
         for(i=1; i<num.length; i++){
@@ -41,5 +42,38 @@ function twoDifferentNumbers(num){
 }
 
 console.log(twoDifferentNumbers("1234567345678"));
+
+// The last number must be even
+function lastNumIsEven(num){
+    lastDigit = num % 10;
+    //   console.log('The last digit of ', num, ' is ', lastDigit);
+    if(lastDigit % 2 == 0){
+        return `${num} is valid`
+    }
+
+   else{
+       return `${num} isn't valid`
+   } 
+}
+console.log(lastNumIsEven("1234567345679"));
+
+// The sum of all the numbers must be greater than 16
+function sum(num){
+    for (let i in num){
+        sum += num[i];
+    } 
+    if(sum >= 16){
+        return true;
+    }
+    else{
+        return false;
+    }    
+}
+console.log(sum("1234567345679"));
+
+
+
+
+
 
 
