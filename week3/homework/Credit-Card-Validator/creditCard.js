3ea3b078e46787bfdc3abe8ef8e268ecdc7aeb4b
sanaz <szeraati69@gmail.com>
2021-10-07 16:31:45 +0330

    finish project

---
+++ week3/homework/Credit-Card-Validator/creditCard.js
@@ -0,0 +1,91 @@
+// Input must be 16 characters
+function numCharacters(num){
+    let input = num.toString().length;
+    
+   if(input == 16){
+        return true;
+   }else{  
+    return false
+   }      
+}
+
+// All characters must be numbers
+function checkNumber(num){
+    let regEx ='^[0-9]+$' ;
+
+    if(num.match(regEx))
+     {
+        return true;
+     }else{        
+        return false
+    } 
+}
+
+// At least two different numbers should be represented
+function twoDifferentNumbers(num){
+    
+        for(i=1; i<num.length; i++){
+           if(num[0] !== num[i])
+              return true;
+        }        
+        return false
+}
+
+// The last number must be even
+function lastNumIsEven(num){
+    lastDigit = num % 10;
+    //   console.log('The last digit of ', num, ' is ', lastDigit);
+    if(lastDigit % 2 == 0){
+        return true;
+    }else{       
+       return false
+   } 
+}
+
+// The sum of all the numbers must be greater than 16
+function sum(num){
+    let sumOfNumbers = 0;
+
+    for (let i = 0 ; i < num.length ; i++){
+        sumOfNumbers += parseInt(num[i]);   
+    } 
+       
+    if(sumOfNumbers > 16){
+        return true;
+    }else{ 
+        return false
+    }      
+}
+
+function validateCreditNumber(num){
+    const answers = [];
+
+    if(numCharacters(num) && checkNumber(num) && twoDifferentNumbers(num) && lastNumIsEven(num) && sum(num) == true){
+       return `your number is valid`
+    }else{
+        if (numCharacters(num) == false) {
+            answers.push("Input must be 16 characters");
+        } 
+    
+        if (checkNumber(num) == false) {
+             answers.push("All characters must be numbers");
+        }
+    
+        if (twoDifferentNumbers(num) == false) {
+            answers.push("At least two different numbers should be represented");
+        }
+    
+        if (lastNumIsEven(num) == false) {
+             answers.push("The last number must be even");
+        } 
+    
+        if (sum(num) == false) {
+             answers.push("The sum of all the numbers must be greater than 16");
+        } 
+
+        const x = answers.join('\n')
+        return `Invalid Number \n ${x} `
+    }  
+}
+
+console.log(validateCreditNumber("1234565675"))
