0bb92e4149d8a22bf3d1ecb8efd78009da72cd99
sanaz <szeraati69@gmail.com>
2021-10-13 19:06:46 +0330

    add ex1-doubleEvenNumbers

---
+++ week4/homework/javascript_exercises/ex1-doubleEvenNumbers.test.js
@@ -0,0 +1,25 @@
+/*------------------------------------------------------------------------------
+Full description at: https://github.com/HackYourFuture/Homework/tree/main/1-JavaScript/Week4#exercise-1-the-odd-ones-out
+
+The `doubleEvenNumbers` function returns only the even numbers in the array 
+passed as the `numbers` parameter and doubles them.
+
+Let's rewrite it (or _refactor_ it, as experienced developers would call it):
+
+- Using the `map` and `filter` functions, rewrite the function body of
+`doubleEvenNumbers`.
+------------------------------------------------------------------------------*/
+// ! Function to be tested
+function doubleEvenNumbers(numbers) {
+  const newNumbers = numbers.filter( number => number % 2 === 0)
+      .map( number => number * 2);
+  return newNumbers;
+}
+
+// ! Unit test (using Jest)
+test('doubleEvenNumbers should take the even numbers and double them', () => {
+  const actual = doubleEvenNumbers([1, 2, 3, 4]);
+  const expected = [4, 8];
+  expect(actual).toEqual(expected);
+});
+
